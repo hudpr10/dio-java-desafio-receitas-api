9d1bbb45a8a6578eccd08f35575aa8bc652441bf
Hudson Reis <hudsonpr09@gmail.com>
2025-07-16 14:38:48 -0300

    feat: RecipeRepository

---
+++ src/main/java/dio/app/domain/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package dio.app.domain.repository;
+
+import dio.app.domain.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
