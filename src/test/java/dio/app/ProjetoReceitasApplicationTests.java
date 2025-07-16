38893e0980ce1828685654908e6a59d6b4a9d548
Hudson Reis <hudsonpr09@gmail.com>
2025-07-16 14:00:56 -0300

    primeiro commit

---
+++ src/test/java/dio/app/ProjetoReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
